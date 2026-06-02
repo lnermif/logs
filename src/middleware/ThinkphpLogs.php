806e82de790425273b37308272b428094bf7e6de
Nermif <cxal@outlook.com>
2026-06-02 09:17:04 +0800

    feat(src): 日志记录器

---
+++ src/middleware/ThinkphpLogs.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nermif\Logs\middleware;
+
+use Nermif\Logs\Logs;
+use Ramsey\Uuid\Uuid;
+use think\Response;
+
+class ThinkphpLogs
+{
+    /**
+     * 处理请求
+     *
+     * @param \think\Request $request
+     * @param \Closure $next
+     * @return Response
+     */
+    public function handle($request, \Closure $next)
+    {
+        Logs::initRequest();
+
+        $response = $next($request);
+
+        if ($response instanceof Response) {
+            $response->header([
+                'X-Request-Id' => Logs::getRequestId()
+            ]);
+        }
+
+        if (PHP_SAPI === 'cli') {
+            Logs::endRequest();
+        }
+
+        return $response;
+    }
+}
